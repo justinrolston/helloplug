1435b1cc93ba923aadf9c2bd6fe862983c51c57f
Justin Rolston <jrolston@covermymeds.com>
2017-01-15 18:02:43 -0500

    init commit

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -1,14 +1,20 @@
 defmodule Helloplug do
-  def init(default_opts) do
-    IO.puts "starting up Helloplug..."
-    default_opts
-  end
+    def call(conn, _opts) do
+    route(conn.method, conn.path_info, conn)
+    end
 
-  def call(conn, _opts) do
-    IO.puts "saying hello!"
-    conn |> 
-        Plug.Conn.put_resp_header("Server", "Plug") |> 
-        Plug.Conn.send_resp(200, "Hello, world!")
-#    Plug.Conn.send_resp(conn, 200, "Hello, Justin!")
-  end
+    def route("GET", ["hello"], conn) do
+    # this route is for /hello
+    conn |> Plug.Conn.send_resp(200, "Hello, world!")
+    end
+
+    def route("GET", ["users", user_id], conn) do
+    # this route is for /users/<user_id>
+    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+    end
+
+    def route(_method, _path, conn) do
+    # this route is called if no other routes match
+    conn |> Plug.Conn.send_resp(404, "Couldn't find that page, sorry!")
+    end
 end
