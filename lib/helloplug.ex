5e4aaa35618802511bc9ddb7ab926f62a06ba7ec
Justin Rolston <jrolston@covermymeds.com>
2017-01-29 15:48:14 -0500

    finished multi routes

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -1,16 +1,3 @@
-defmodule Router do
-  defmacro __using__(_opts) do
-    quote do
-      def init(options) do
-        options
-      end
-      def call(conn, _opts) do
-        route(conn.method, conn.path_info, conn)
-      end
-    end
-  end
-end
-
 defmodule Helloplug do
     use Router
 
