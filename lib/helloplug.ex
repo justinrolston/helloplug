5b0ff4e4ad5860f9bd5228fd93a2ae589a4e5e77
Justin Rolston <jrolston@covermymeds.com>
2017-01-29 16:06:02 -0500

    added templates

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -6,9 +6,13 @@ defmodule Helloplug do
       conn |> Plug.Conn.send_resp(200, "Hello, world!")
     end
 
+    require EEx # you have to require EEx before using its macros outside of functions
+    EEx.function_from_file :defp, :template_show_user, "templates/show_user.eex", [:user_id]
     def route("GET", ["users", user_id], conn) do
-      # this route is for /users/<user_id>
-      conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+      page_contents = template_show_user(user_id)
+      conn 
+        |> Plug.Conn.put_resp_content_type("text/html") 
+        |> Plug.Conn.send_resp(200, page_contents)
     end
 
     def route(_method, _path, conn) do
